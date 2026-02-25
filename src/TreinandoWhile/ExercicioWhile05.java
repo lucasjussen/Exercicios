2b61f5d6e3d68a45b52f6cf4395c98be377cdeb9
Lucas Jussen <lucassjussen@gmail.com>
2026-02-24 23:57:51 -0300

    Treinando Laços de repetição!

---
+++ src/TreinandoWhile/ExercicioWhile05.java
@@ -0,0 +1,12 @@
+package TreinandoWhile;
+
+public class ExercicioWhile05 {
+    public static void main(String[] args) {
+        int numero = 50;
+
+        for(int i = 0; i <= numero; i += 5) {
+            System.out.println(i);
+        }
+
+    }
+}
