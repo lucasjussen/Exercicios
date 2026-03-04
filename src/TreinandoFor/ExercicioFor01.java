8b132d773954c07cf1f689f3c8dcc3a23558fbb8
Lucas Jussen <lucassjussen@gmail.com>
2026-03-03 23:05:25 -0300

    Treinando Laços de repetição novamente..

---
+++ src/TreinandoFor/ExercicioFor01.java
@@ -0,0 +1,10 @@
+package TreinandoFor;
+
+public class ExercicioFor01 {
+    public static void main(String[] args) {
+
+        for(int i = 1; i <= 50; i++){
+            System.out.println(i);
+        }
+    }
+}
---
+++ src/TreinandoWhile/ExercicioWhile09.java
@@ -0,0 +1,7 @@
+package TreinandoWhile;
+
+public class ExercicioWhile09 {
+    public static void main(String[] args) {
+        int senha = 20;
+    }
+}
