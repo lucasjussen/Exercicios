ada2b39df588a5bb09c7db96ab00c074411a71e1
Lucas Jussen <lucassjussen@gmail.com>
2026-04-19 23:40:11 -0300

    Treinando antes de chegar em POO

--- src/TreinandoAntesDeComecarPOO/ExercicioTiposPrimitivos02.java
+++ src/TreinandoAntesDeComecarPOO/ExercicioTiposPrimitivos02.java
@@ -7,16 +7,25 @@ public class ExercicioTiposPrimitivos02 {
         byte filhos = 2;
         float altura = 1.75F;
         boolean empregado = true;
+
         double salarioAumento;
         double filhoGasto;
 
         salarioAumento = salario * 1.10;
-        filhoGasto = salario / filhos;
 
-        System.out.println("idade: " + idade + " Salário: " + salario + " filhos: " + filhos +" Altura: " + altura+ " Empregado: "+empregado);
+        if (filhos > 0) {
+            filhoGasto = salario / filhos;
+        } else {
+            filhoGasto = 0;
+        }
+
+        System.out.println("Idade: " + idade +
+                " Salário: " + salario +
+                " Filhos: " + filhos +
+                " Altura: " + altura +
+                " Empregado: " + empregado);
 
         System.out.println("Salário com aumento: " + salarioAumento);
         System.out.println("Gasto por filho: " + filhoGasto);
-
     }
 }
